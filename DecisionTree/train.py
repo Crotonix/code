a9a4fc1197510d5874514980962744fa5455493f
Crotonix <saurabhkumawat57@gmail.com>
2024-11-09 12:26:57 +0530

    reproduced training results

--- DecisionTree/train.py
+++ DecisionTree/train.py
@@ -10,28 +10,74 @@ x = np.array(data.data)
 y = np.array(data.target)
 
 x_train, x_test, y_train, y_test = model_selection.train_test_split(
-    x, y, test_size=0.2, random_state=0
+    x, y, test_size=0.25, random_state=0
 )
-print("Train shape:", x_train.shape)
-print("Test Shape:", x_test.shape)
+print("Train shape for iris:", x_train.shape)
+print("Test Shape for iris:", x_test.shape)
 
+print("#########################IRIS######################")
+
+print("-------------DecisionTree----------------")
 
 my_tree = DecisionTree(max_depth=4, min_samples_leaf=1)
+my_tree.train(x_train, y_train)
+
+train_preds = my_tree.predict(x_set=x_train)
+print("Train Performance")
+print(
+    "Train size",
+    len(y_train),
+    "True preds",
+    sum(train_preds == y_train),
+    "Train accuracy",
+    sum(train_preds == y_train) / len(y_train),
+)
+
+test_preds = my_tree.predict(x_set=x_test)
+print("Test Performance")
+print(
+    "Test size",
+    len(y_test),
+    "True preds",
+    sum(test_preds == y_test),
+    "Test accuracy",
+    sum(test_preds == y_test) / len(y_test),
+)
+
+
+print("----------------RF---------------")
+
+my_tree = RandomForestClassfier(
+    n_base_learner=50,
+    numb_of_features_splitting="sqrt",
+)
 my_tree.train(x_train, y_train)
 
 
 train_preds = my_tree.predict(x_set=x_train)
 print("Train Performance")
-print("Train size", len(y_train))
-print("True preds", sum(train_preds == y_train))
-print("Train accuracy", sum(train_preds == y_train) / len(y_train))
+print(
+    "Train size",
+    len(y_train),
+    "True preds",
+    sum(train_preds == y_train),
+    "Train accuracy",
+    sum(train_preds == y_train) / len(y_train),
+)
 
 test_preds = my_tree.predict(x_set=x_test)
 print("Test Performance")
-print("Test size", len(y_test))
-print("True preds", sum(test_preds == y_test))
-print("Test accuracy", sum(test_preds == y_test) / len(y_test))
+print(
+    "Test size",
+    len(y_test),
+    "True preds",
+    sum(test_preds == y_test),
+    "Test accuracy",
+    sum(test_preds == y_test) / len(y_test),
+)
 
+
+print("######################Breast#############################")
 
 data = datasets.load_breast_cancer()
 
@@ -41,24 +87,63 @@ y = np.array(data.target)
 x_train, x_test, y_train, y_test = model_selection.train_test_split(
     x, y, test_size=0.2, random_state=0
 )
-print("Train shape:", x_train.shape)
-print("Test Shape:", x_test.shape)
+print("Train shape for breast:", x_train.shape)
+print("Test Shape for breast:", x_test.shape)
+
+print("-------------DecisionTree----------------")
+
+my_tree = DecisionTree(max_depth=4, min_samples_leaf=5, min_infromation_gain=0.05)
+my_tree.train(x_train, y_train)
+
+train_preds = my_tree.predict(x_set=x_train)
+print("Train Performance")
+print(
+    "Train size",
+    len(y_train),
+    "True preds",
+    sum(train_preds == y_train),
+    "Train accuracy",
+    sum(train_preds == y_train) / len(y_train),
+)
+
+test_preds = my_tree.predict(x_set=x_test)
+print("Test Performance")
+print(
+    "Test size",
+    len(y_test),
+    "True preds",
+    sum(test_preds == y_test),
+    "Test accuracy",
+    sum(test_preds == y_test) / len(y_test),
+)
+
+
+print("----------------RF---------------")
 
 my_tree = RandomForestClassfier(
-    numb_of_features_splitting=3,
-    bootstrap_sample_size=20,
+    n_base_learner=100, max_depth=4, min_samples_leaf=5, min_information_gain=0.05
 )
 my_tree.train(x_train, y_train)
 
 
 train_preds = my_tree.predict(x_set=x_train)
 print("Train Performance")
-print("Train size", len(y_train))
-print("True preds", sum(train_preds == y_train))
-print("Train accuracy", sum(train_preds == y_train) / len(y_train))
+print(
+    "Train size",
+    len(y_train),
+    "True preds",
+    sum(train_preds == y_train),
+    "Train accuracy",
+    sum(train_preds == y_train) / len(y_train),
+)
 
 test_preds = my_tree.predict(x_set=x_test)
 print("Test Performance")
-print("Test size", len(y_test))
-print("True preds", sum(test_preds == y_test))
-print("Test accuracy", sum(test_preds == y_test) / len(y_test))
+print(
+    "Test size",
+    len(y_test),
+    "True preds",
+    sum(test_preds == y_test),
+    "Test accuracy",
+    sum(test_preds == y_test) / len(y_test),
+)
